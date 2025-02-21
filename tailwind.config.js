3220aa781a567d37a2ad118eee9ca60a7e890d7d
Jhon Nova <jhonnovax@gmail.com>
2025-02-21 07:32:37 -0800

    Customize Tailwind theme with FortuneOK brand colors
    - Set custom primary colors for light and dark themes
    - Preserve base theme configurations from DaisyUI
    - Ensure consistent branding across light and dark modes

--- tailwind.config.js
+++ tailwind.config.js
@@ -63,6 +63,17 @@ module.exports = {
     // You can add another theme among the list of 30+
     // Add "data-theme='theme_name" to any HTML tag to enable the 'theme_name' theme.
     // https://daisyui.com/
-    themes: ["light", "dark"],
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#006e00",
+        },
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          primary: "#007B00",
+        },
+      }
+    ]
   },
 };
