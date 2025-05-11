b9797f5cc9926cccc330027ebe0456418d646830
Jhon Nova <jhonnovax@gmail.com>
2025-05-11 08:40:48 -0700

    Update configuration and documentation to reflect rebranding from ShipFast to FortuneOK; modify URLs and comments in various files for consistency with new branding.

--- app/page.js
+++ app/page.js
@@ -16,7 +16,6 @@ export const dynamic = "force-dynamic";
 
 // This is a private page: It's protected by the layout.js component which ensures the user is authenticated.
 // It's a server compoment which means you can fetch data (like the user profile) before the page is rendered.
-// See https://shipfa.st/docs/tutorials/private-page
 export default function Dashboard() {
   const { appName, appDescription } = config;
 
