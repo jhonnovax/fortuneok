4d4d487f99f8a91695e47c9c13f7f3787502ec0d
Jhon Nova <jhonnovax@gmail.com>
2025-05-10 20:34:28 -0700

    Update button visibility in Dashboard and RightSidebar components for improved responsiveness; add label to Add Investment button for better accessibility.

--- components/dashboard/RightSidebar.js
+++ components/dashboard/RightSidebar.js
@@ -11,10 +11,11 @@ export default function RightSidebar({ children, onAddInvestment }) {
             </svg>
             My Assets
 
-            <button className="btn btn-primary ml-auto btn-sm" onClick={onAddInvestment} title="Add Investment">
+            <button className="btn btn-primary ml-auto btn-sm" onClick={onAddInvestment}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
               </svg>
+              <span>Add Asset</span>
             </button>
           </h2>
         </div>
