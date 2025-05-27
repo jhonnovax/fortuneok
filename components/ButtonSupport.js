2cdba35e04c9e8c3cb1d3a08603cf225272f7a2b
Jhon Nova <jhonnovax@gmail.com>
2025-05-26 19:38:58 -0700

    Refactor package dependencies by removing unused packages and updating package-lock.json and package.json for improved performance and reduced bundle size. Simplify ButtonSupport component by removing Crisp chat integration and streamlining the email support functionality. Clean up LayoutClient component by removing Crisp chat support logic and unnecessary imports, enhancing overall code clarity.

--- components/ButtonSupport.js
+++ components/ButtonSupport.js
@@ -1,23 +1,18 @@
 "use client";
 
-import { Crisp } from "crisp-sdk-web";
 import config from "@/config";
 
 // Use this button if chat is hidden on some routes. config.js has onlyShowOnRoutes set to ["/"] so it will be hidden on all routes except the home page.
 // If Crisp is not enable, it will open the support email in the default email client.
 const ButtonSupport = () => {
-  const handleClick = () => {
-    if (config.crisp?.id) {
-      Crisp.chat.show();
-      Crisp.chat.open();
-    } else if (config.resend?.supportEmail) {
-      // open default email client in new window with "need help with ${config.appName}" as subject
-      window.open(
-        `mailto:${config.resend.supportEmail}?subject=Need help with ${config.appName}`,
-        "_blank"
-      );
-    }
-  };
+
+  function handleClick() {
+    // open default email client in new window with "need help with ${config.appName}" as subject
+    window.open(
+      `mailto:${config.resend.supportEmail}?subject=Need help with ${config.appName}`,
+      "_blank"
+    );
+  }
 
   return (
     <button
@@ -42,6 +37,7 @@ const ButtonSupport = () => {
       Support
     </button>
   );
+
 };
 
 export default ButtonSupport;
