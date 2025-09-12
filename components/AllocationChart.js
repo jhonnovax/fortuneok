2a09eaea0a410986751d63309868c599616cfbc3
Jhon Nova <jhonnovax@gmail.com>
2025-09-12 15:12:47 -0500

    Refactor AllocationChart component to modularize total assets display by integrating TotalAssetsByCurrency component. Removed redundant calculations and improved code clarity, enhancing overall maintainability and user experience.

--- components/AllocationChart.js
+++ components/AllocationChart.js
@@ -8,15 +8,14 @@ import {
   Tooltip,
 } from 'recharts';
 import { useMemo } from 'react';
-import { formatCurrency, formatFullCurrency, formatPercentage, maskValue } from '../services/intlService';
+import { formatCurrency, formatPercentage, maskValue } from '../services/intlService';
 import ErrorLoadingData from './ErrorLoadingData';
 import { getChartColors } from '../services/chartService';
 import { BREAKPOINTS } from '@/services/breakpointService';
 import { useTailwindBreakpoint } from '@/hooks/useTailwindBreakpoint';
 import { useSystemTheme } from '@/hooks/useSystemTheme';
-import currencies from '@/public/currencies.json';
-import CurrencyBadge from './CurrencyBadge';
 import { ASSET_CATEGORIES } from '../services/assetService';
+import TotalAssetsByCurrency from './TotalAssetsByCurrency';
 
 // Custom tooltip component
 const CustomTooltip = ({ active, payload, showValues }) => {
@@ -45,28 +44,6 @@ export default function AllocationChart({ isLoading, error, baseCurrency, filter
   const chartColors = getChartColors(theme);
   const isDesktopOrUpper = breakpointInPixels >= BREAKPOINTS.LG;
 
-  const assetsValuesByCurrency = useMemo(() => {
-    const isCategory = filteredAssetData.some(asset => asset.assets?.length > 1);
-    const assetsInCategories = isCategory ? filteredAssetData.flatMap(asset => asset.assets) : filteredAssetData;
-
-    return Object.groupBy(assetsInCategories, asset => asset.currentValuation?.currency);
-  }, [filteredAssetData]);
-
-  const totalAssetsbyCurrency = useMemo(() => {
-    const totalAssetsbyCurrency = Object.keys(assetsValuesByCurrency).map(currency => {
-      const totalValue = assetsValuesByCurrency[currency].reduce((acc, asset) => acc + asset.valuationInPreferredCurrency, 0);
-
-      return {
-        currency,
-        percentage: totalValue / totalAssetsValue,
-        totalValue
-      }
-    });
-    const totalAssetsbyCurrencyOrdered = totalAssetsbyCurrency.sort((a, b) => b.totalValue - a.totalValue);
-
-    return totalAssetsbyCurrencyOrdered;
-  }, [assetsValuesByCurrency, totalAssetsValue]);
-  
   // Add fill property to data for tooltip color  
   const assetDataWithFill = useMemo(() => {
     return filteredAssetData.map((item, index) => ({
@@ -182,20 +159,12 @@ export default function AllocationChart({ isLoading, error, baseCurrency, filter
         </h3>
 
       {/* Total Assets Values by Currency */}
-      <div className="p-4 md:p-6 pb-0 flex items-center justify-center flex-wrap gap-2">
-        {totalAssetsbyCurrency.map(value => (
-          <div className="inline-block" key={value.currency}>
-            <div className="p-1 border border-base-content/10 rounded-lg shadow-sm bg-base-100 text-xs md:text-sm md:text-base">
-              <div className="flex items-center">
-                <span className="font-bold mr-1">{formatPercentage(value.percentage * 100, 2)}</span> in <CurrencyBadge currencyCode={value.currency} />
-              </div>
-              <div className="text-center">
-                {currencies.find(currency => currency.code === baseCurrency)?.flag}{baseCurrency} {showValues ? formatFullCurrency(value.totalValue) : maskValue(value.totalValue)}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <TotalAssetsByCurrency 
+        baseCurrency={baseCurrency} 
+        filteredAssetData={filteredAssetData} 
+        totalAssetsValue={totalAssetsValue} 
+        showValues={showValues} 
+      />
 
       {/* Chart */}
       <div className="card-body p-0 h-[300px] lg:h-[400px] items-center justify-center">
---
+++ components/TotalAssetsByCurrency.js
@@ -0,0 +1,47 @@
+import { formatPercentage, formatFullCurrency, maskValue } from '@/services/intlService';
+import CurrencyBadge from './CurrencyBadge';
+import currencies from '@/public/currencies.json';
+import { useMemo } from 'react';
+
+export default function TotalAssetsByCurrency({ baseCurrency, filteredAssetData, totalAssetsValue, showValues }) {
+
+  const assetsValuesByCurrency = useMemo(() => {
+    const isCategory = filteredAssetData.some(asset => asset.assets?.length > 1);
+    const assetsInCategories = isCategory ? filteredAssetData.flatMap(asset => asset.assets) : filteredAssetData;
+
+    return Object.groupBy(assetsInCategories, asset => asset.currentValuation?.currency);
+  }, [filteredAssetData]);
+
+  const totalAssetsbyCurrency = useMemo(() => {
+    const totalAssetsbyCurrency = Object.keys(assetsValuesByCurrency).map(currency => {
+      const totalValue = assetsValuesByCurrency[currency].reduce((acc, asset) => acc + asset.valuationInPreferredCurrency, 0);
+
+      return {
+        currency,
+        percentage: totalValue / totalAssetsValue,
+        totalValue
+      }
+    });
+    const totalAssetsbyCurrencyOrdered = totalAssetsbyCurrency.sort((a, b) => b.totalValue - a.totalValue);
+
+    return totalAssetsbyCurrencyOrdered;
+  }, [assetsValuesByCurrency, totalAssetsValue]);
+
+  return (
+    <div className="p-4 md:p-6 pb-0 flex items-center justify-center flex-wrap gap-2">
+      {totalAssetsbyCurrency.map(value => (
+        <div className="inline-block" key={value.currency}>
+          <div className="p-1 border border-base-content/10 rounded-lg shadow-sm bg-base-100 text-xs md:text-sm md:text-base">
+            <div className="flex items-center">
+              <span className="font-bold mr-1">{formatPercentage(value.percentage * 100, 2)}</span> in <CurrencyBadge currencyCode={value.currency} />
+            </div>
+            <div className="text-center">
+              {currencies.find(currency => currency.code === baseCurrency)?.flag}{baseCurrency} {showValues ? formatFullCurrency(value.totalValue) : maskValue(value.totalValue)}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+
+}
