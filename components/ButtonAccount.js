4c76eb3c2c37eb920cf14049d58b432995b1c8a6
Jhon Nova <jhonnovax@gmail.com>
2025-10-11 11:44:49 -0500

    Refactor ButtonAccount component to improve dropdown handling and enhance UI interactivity. This update introduces a dedicated function for toggling the dropdown state, ensuring better accessibility and responsiveness in line with the design system.

--- components/ButtonAccount.js
+++ components/ButtonAccount.js
@@ -35,6 +35,11 @@ const ButtonAccount = ({ onAddAsset }) => {
     setIsLoading(false);
   };
 
+  function handleDropdown(e) {
+    e.stopPropagation(); // Stop DaisyUI's dropdown wrapper has built-in behavior
+    setOpenDropdown(!openDropdown);
+  }
+
   // Don't show anything if not authenticated (we don't have any info about the user)
   if (status === "unauthenticated") {
     return null;
@@ -42,7 +47,7 @@ const ButtonAccount = ({ onAddAsset }) => {
 
   return (
     <div className="dropdown dropdown-end">
-      <button type="button" className="btn btn-tertiary" onClick={() => setOpenDropdown(!openDropdown)}>
+      <button type="button" className="btn btn-tertiary" onClick={handleDropdown}>
         {session?.user?.image ? (
               <img
                 src={session?.user?.image}
@@ -83,46 +88,44 @@ const ButtonAccount = ({ onAddAsset }) => {
               </svg>
             )}
       </button>
-      {openDropdown && (
-        <ul className={`dropdown-content z-[1] menu p-2 shadow-lg bg-base-100 rounded-box w-full min-w-40 mt-1 ${openDropdown ? 'block' : 'hidden'}`}>
-          <li>
-            <button
-              className="flex items-center gap-2 hover:bg-base-300 duration-200 py-1.5 px-4 w-full rounded-lg font-medium"
-              onClick={onAddAsset}
+      <ul className={`dropdown-content z-[1] menu p-2 shadow-lg bg-base-100 rounded-box w-full min-w-40 mt-1 ${openDropdown ? 'block' : 'hidden'}`}>
+        <li>
+          <button
+            className="flex items-center gap-2 hover:bg-base-300 duration-200 py-1.5 px-4 w-full rounded-lg font-medium"
+            onClick={onAddAsset}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            Add Asset
+          </button>
+        </li>
+        <li>
+          <button
+            className="flex items-center gap-2 hover:bg-error/20 hover:text-error duration-200 py-1.5 px-4 w-full rounded-lg font-medium"
+            onClick={handleSignOut}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-              </svg>
-              Add Asset
-            </button>
-          </li>
-          <li>
-            <button
-              className="flex items-center gap-2 hover:bg-error/20 hover:text-error duration-200 py-1.5 px-4 w-full rounded-lg font-medium"
-              onClick={handleSignOut}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"
-                  clipRule="evenodd"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M6 10a.75.75 0 01.75-.75h9.546l-1.048-.943a.75.75 0 111.004-1.114l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 11-1.004-1.114l1.048-.943H6.75A.75.75 0 016 10z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              Logout
-            </button>
-          </li>
-        </ul>
-      )}
+              <path
+                fillRule="evenodd"
+                d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M6 10a.75.75 0 01.75-.75h9.546l-1.048-.943a.75.75 0 111.004-1.114l2.5 2.25a.75.75 0 010 1.114l-2.5 2.25a.75.75 0 11-1.004-1.114l1.048-.943H6.75A.75.75 0 016 10z"
+                clipRule="evenodd"
+              />
+            </svg>
+            Logout
+          </button>
+        </li>
+      </ul>
     </div>
   )
 };
