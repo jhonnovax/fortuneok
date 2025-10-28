a81b6c88177089a5e017bb84a770902fd9008b26
Jhon Nova <jhonnovax@gmail.com>
2025-10-28 14:34:29 -0500

    Refactor DashboardLayout component by simplifying its structure to directly render children, removing unnecessary imports and SEO metadata. This streamlines the layout for better performance and maintainability.

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,31 +1,9 @@
-import "@/app/globals.css";
-import { getSEOTags } from "@/libs/seo";
-import ClientLayout from "@/components/LayoutClient";
-import config from "@/config";
+export default function DashboardLayout({ children }) {
 
-export const viewport = {
-	// Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
-	themeColor: config.colors.main,
-	width: "device-width",
-	initialScale: 1,
-};
+  return (
+    <>
+      {children}
+    </>
+  );
 
-// This adds default SEO tags to all pages in our app.
-// You can override them in each page passing params to getSOTags() function.
-export const metadata = getSEOTags();
-
-export default function RootLayout({ children }) {
-	return (
-		<html
-			lang="en"
-			data-theme={config.colors.theme}
-		>
-			<body>
-				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
-				<ClientLayout>
-					{children}
-				</ClientLayout>
-			</body>
-		</html>
-	);
 }
