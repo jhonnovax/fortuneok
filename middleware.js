1d0a34b6ee6a6aa42e6c2157768fa65b1254b615
Jhon Nova <jhonnovax@gmail.com>
2025-10-29 09:19:34 -0500

    Update middleware to include NEXTAUTH_SECRET for token retrieval and adjust redirect URLs to use req.nextUrl.origin for consistency; enhances security and improves routing logic.

--- middleware.js
+++ middleware.js
@@ -2,17 +2,17 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  const token = await getToken({ req });
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const { pathname } = req.nextUrl;
 
   // If user is signed in and visiting "/", redirect to /dashboard
   if (token && pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+    return NextResponse.redirect(new URL("/dashboard", req.nextUrl.origin ));
   }
 
   // If not signed in and trying to access protected route, redirect to /login
   /* if (!token && pathname.startsWith("/dashboard")) {
-    return NextResponse.redirect(new URL("/api/auth/signin", req.url));
+    return NextResponse.redirect(new URL("/api/auth/signin", req.nextUrl.origin ));
   } */
 
   return NextResponse.next();
